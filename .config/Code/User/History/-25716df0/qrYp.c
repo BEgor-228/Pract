fb2ee3befe8c52dd3b84f673835c66f772a2dfcf
Egor Bezrukavnikov <bezrukavnikov@gmail.com>
2025-07-13 13:14:11 +0300

    снял отслеживание со всех файлов

--- .config/Code/User/History/-25716df0/qrYp.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-int main(int argc, char* argv[], char* envp[]){
-    char program [] = "ls";
-    char* arg_list [10] = {"ls","/home/user"};
-    execvp (program,arg_list);
-    printf("Error on program start");
-    return 123;
-}
--- .config/Code/User/History/6e490074/goa0.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-int main(){
-    printf("Process ID: %d\n", (int) getpid());
-    printf("Parent process ID: %d\n", (int) getppid());
-    getchar();
-    return 0;
-}
